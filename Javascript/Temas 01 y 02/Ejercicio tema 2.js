3f39844b96b4eaf0875b51ce62b50dfa6cab5953
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2023-03-11 12:10:09 +0100

    Ejercicio tema 2

---
+++ Javascript/Temas 01 y 02/Ejercicio tema 2.js
@@ -0,0 +1,14 @@
+// Ejercicio tema 2
+
+let lista = [
+    "Gonzalo",
+    35,
+    true,
+    new Date('May, 23, 1987'),
+    {
+        título: "El Nombre del Viento",
+        autor: "Patrick Rothfuss",
+        fecha: new Date(March, 27, 2007),
+        url: "https://es.wikipedia.org/wiki/El_nombre_del_viento",
+    }
+]
